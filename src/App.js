a10e05ac58837fe65e399f1150efe7db52800732
antoniotex <acm.skt@gmail.com>
2018-11-13 00:17:41 -0200

    Adiciona AppContainer ao componente principal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import AppContainer from './components/container/app-container'
 import './assets/styles/scss/app.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <AppContainer />
     );
   }
 }
--- src/components/container/app-container.js
+++ src/components/container/app-container.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
-const AppContainer = () => {
+const AppContainer = () => (
     <div>Container</div>
-}
+)
+
+export default AppContainer
