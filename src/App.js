90a7d08016e0c2cb913598289e64514af677adad
antoniotex <acm.skt@gmail.com>
2018-11-13 00:09:20 -0200

    Adiciona script SASS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import './assets/styles/scss/app.scss';
 
 class App extends Component {
   render() {
