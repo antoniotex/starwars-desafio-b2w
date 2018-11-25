9f27545e0e831aef1fa09ad316c0235ec02ba8bd
antoniotex <acm.skt@gmail.com>
2018-11-25 01:43:01 -0200

    Testa função de gerar numero randômico

--- src/app.test.js
+++ src/app.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App renders without crashing', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
+
+describe('Random Number Test', () => {
+  it('should return number greater than or equal to 1', () => {
+    expect(App.prototype.getRandomNumber(61)).toBeGreaterThanOrEqual(1)
+  });
+  it('should return number less than or equal to 61', () => {
+    expect(App.prototype.getRandomNumber(61)).toBeLessThanOrEqual(61)
+  });
+})
